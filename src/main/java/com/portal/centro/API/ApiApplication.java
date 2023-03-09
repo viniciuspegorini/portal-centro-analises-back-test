29db41843ffa10235e1fcbc4031542c50a5cb6f4
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-08 22:06:02 -0300

    after success init message

--- src/main/java/com/portal/centro/API/ApiApplication.java
+++ src/main/java/com/portal/centro/API/ApiApplication.java
@@ -1,13 +1,22 @@
 package com.portal.centro.API;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Slf4j
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void startupMessage() {
+		log.info("successfully compiled and started up");
 	}
 
 }
