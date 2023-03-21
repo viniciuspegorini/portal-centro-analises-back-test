c2641361cb98d22e96b2d77b6f95b226bb959b9b
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-20 21:33:21 -0300

    config provider

---
+++ src/main/java/com/portal/centro/API/provider/ConfigProvider.java
@@ -0,0 +1,13 @@
+package com.portal.centro.API.provider;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "any")
+public class ConfigProvider {
+
+    private String any;
+}
