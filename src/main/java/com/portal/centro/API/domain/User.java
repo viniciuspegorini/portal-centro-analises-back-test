0d9ef0d3c4aa1d1b192f4defd5fd45bbc04aba81
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-03-20 21:29:14 -0300

    user permissions

--- src/main/java/com/portal/centro/API/domain/User.java
+++ src/main/java/com/portal/centro/API/domain/User.java
@@ -2,11 +2,17 @@ package com.portal.centro.API.domain;
 
 import com.portal.centro.API.generic.IModel;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Entity(name = "_user")
 @Table(uniqueConstraints = {
@@ -17,7 +23,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class User extends IModel {
+public class User extends IModel implements UserDetails {
 
     @Column(name = "username")
     @Getter
@@ -42,4 +48,39 @@ public class User extends IModel {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id")
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> list = new ArrayList<>();
+        list.addAll(this.authorities);
+        return list;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authorities",
+            joinColumns = @JoinColumn(
+                    name = "tb_user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "authority_id", referencedColumnName = "id"))
+    private Set<Authority> authorities;
 }
