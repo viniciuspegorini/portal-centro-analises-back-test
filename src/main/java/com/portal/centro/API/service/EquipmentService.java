0274e1aa50cb4e1712d0f82839e722eb6011a7ff
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2023-04-05 20:32:30 -0300

    adicao crud techinicalReport

--- src/main/java/com/portal/centro/API/service/EquipmentService.java
+++ src/main/java/com/portal/centro/API/service/EquipmentService.java
@@ -4,18 +4,14 @@ import com.portal.centro.API.generic.crud.GenericService;
 import com.portal.centro.API.model.Equipment;
 import com.portal.centro.API.repository.EquipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EquipmentService extends GenericService<Equipment, Long> {
 
-    BCryptPasswordEncoder passwordEncoder;
-
     @Autowired
     public EquipmentService(EquipmentRepository equipmentRepository) {
         super(equipmentRepository);
-        passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
