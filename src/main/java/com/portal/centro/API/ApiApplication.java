bcf6d0750a39958d409c54161c2c9fc6cd6e1c84
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-04-12 21:20:27 -0300

    disable cors for front local

--- src/main/java/com/portal/centro/API/ApiApplication.java
+++ src/main/java/com/portal/centro/API/ApiApplication.java
@@ -4,7 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @Slf4j
@@ -17,6 +21,17 @@ public class ApiApplication {
 	@EventListener(ApplicationReadyEvent.class)
 	public void startupMessage() {
 		log.info("successfully compiled and started up");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("PUT", "DELETE",
+						"GET", "POST");
+			}
+		};
 	}
 
 }
