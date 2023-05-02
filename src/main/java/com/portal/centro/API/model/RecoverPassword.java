c6fc5355b8bae4830c82086f02cbd3529560f63f
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-05-02 19:17:26 -0300

    added RecoverPassword models

---
+++ src/main/java/com/portal/centro/API/model/RecoverPassword.java
@@ -0,0 +1,19 @@
+package com.portal.centro.API.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecoverPassword {
+
+    private String username;
+    private Integer code;
+    private LocalDateTime dateTime;
+
+}
