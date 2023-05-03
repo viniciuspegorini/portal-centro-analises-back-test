375c930b0f2fc8a125e1ad33f42b39b9c7f7bacb
marco-rozo <marcorozo99@gmail.com>
2023-05-03 20:42:01 -0300

    chore: send email to user

--- src/main/java/com/portal/centro/API/service/EmailCodeService.java
+++ src/main/java/com/portal/centro/API/service/EmailCodeService.java
@@ -1,8 +1,10 @@
 package com.portal.centro.API.service;
 
+import com.portal.centro.API.dto.EmailDto;
 import com.portal.centro.API.generic.crud.GenericService;
 import com.portal.centro.API.model.EmailCode;
 import com.portal.centro.API.model.User;
+import com.portal.centro.API.provider.ConfigFrontProvider;
 import com.portal.centro.API.repository.EmailCodeRepository;
 import com.portal.centro.API.repository.UserRepository;
 import com.portal.centro.API.utils.UtilsService;
@@ -15,15 +17,19 @@ import java.time.LocalDateTime;
 @Service
 public class EmailCodeService extends GenericService<EmailCode, Long> {
     private final HashingService hashingService;
+    private final EmailService emailService;
+    private final ConfigFrontProvider configFrontProvider;
     private final EmailCodeRepository emailCodeRepository;
     private final UserRepository userRepository;
 
     @Autowired
-    public EmailCodeService(EmailCodeRepository emailCodeRepository, HashingService hashingService, UserRepository userRepository) {
+    public EmailCodeService(EmailCodeRepository emailCodeRepository, HashingService hashingService, UserRepository userRepository, EmailService emailService, ConfigFrontProvider configFrontProvider) {
         super(emailCodeRepository);
         this.hashingService = hashingService;
         this.emailCodeRepository = emailCodeRepository;
         this.userRepository = userRepository;
+        this.emailService = emailService;
+        this.configFrontProvider = configFrontProvider;
     }
 
     @Override
@@ -38,6 +44,15 @@ public class EmailCodeService extends GenericService<EmailCode, Long> {
         EmailCode emailCode = EmailCode.builder().code(hashKey).user(user).createdAt(dateTime).build();
 
         //ENVIO DE EMAIL
+        EmailDto emailDto = new EmailDto();
+        emailDto.setEmailTo(user.getEmail());
+        emailDto.setSubject("Confirmação de email - LAB CA");
+        emailDto.setSubjectBody("Codigo para confirmação");
+        String link = configFrontProvider.getBaseurl() + configFrontProvider.getPort() + configFrontProvider.getEmailconfirm() + "/" + hashKey;
+        emailDto.setContentBody("<p>Clique aqui para confirmar seu email: <a href="+link+">CONFIRMAR</a>.</p></br>Caso nao consiga clicar, copie e cole a URl abaixo no seu navegador:" + link);
+
+        emailService.sendEmail(emailDto);
+
         return this.save(emailCode);
     }
 
