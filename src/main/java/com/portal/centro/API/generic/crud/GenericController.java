b33063b84d9ed9ee0bcb7086c273f68e6347e2e7
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-05-21 20:18:34 -0300

    chore: add page, add specification

--- src/main/java/com/portal/centro/API/generic/crud/GenericController.java
+++ src/main/java/com/portal/centro/API/generic/crud/GenericController.java
@@ -1,10 +1,20 @@
 package com.portal.centro.API.generic.crud;
 
+import com.google.common.base.Joiner;
+import com.portal.centro.API.enums.SearchOperation;
+import com.portal.centro.API.generic.specification.GenericSpecification;
+import com.portal.centro.API.generic.specification.GenericSpecificationsBuilder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.Serializable;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class GenericController<T, ID extends Serializable> {
 
@@ -36,6 +46,48 @@ public abstract class GenericController<T, ID extends Serializable> {
     @GetMapping
     public ResponseEntity getAll() throws Exception {
         return ResponseEntity.ok(genericService.getAll());
+    }
+
+    @GetMapping("page")
+    public List<T> search(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "size") Integer size,
+            @RequestParam(value = "order",required = false) String order,
+            @RequestParam(value = "sort",required = false) Boolean asc
+    ) throws Exception {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        if (order != null && asc != null) {
+            pageRequest = PageRequest.of(page, size,
+                    asc ? Sort.Direction.ASC : Sort.Direction.DESC, order);
+        }
+        return genericService.page(pageRequest);
+    }
+
+    @GetMapping("search")
+    public List<T> search(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "size") Integer size,
+            @RequestParam(value = "order",required = false) String order,
+            @RequestParam(value = "sort",required = false) Boolean asc,
+            @RequestParam(value = "search", required = false) String search
+    ) throws Exception {
+        Specification<T> spec = resolveSpecification(search);
+        PageRequest pageRequest = PageRequest.of(page, size);
+        if (order != null && asc != null) {
+            pageRequest = PageRequest.of(page, size,
+                    asc ? Sort.Direction.ASC : Sort.Direction.DESC, order);
+        }
+        return genericService.search(spec, pageRequest);
+    }
+
+    protected Specification<T> resolveSpecification(String searchParameters) {
+        GenericSpecificationsBuilder<T> specBuilder = new GenericSpecificationsBuilder<>();
+        Pattern pattern = Pattern.compile("(\\p{Punct}?)(\\w+?)(" + Joiner.on("|").join(SearchOperation.SIMPLE_OPERATION_SET) + ")(\\p{Punct}?)(\\w+?)(\\p{Punct}?),");
+        Matcher matcher = pattern.matcher(searchParameters + ",");
+        while (matcher.find()) {
+            specBuilder.with(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(5), matcher.group(4), matcher.group(6));
+        }
+        return specBuilder.build(GenericSpecification<T>::new);
     }
 
     @PostMapping("{id}")
--- src/main/java/com/portal/centro/API/generic/crud/GenericService.java
+++ src/main/java/com/portal/centro/API/generic/crud/GenericService.java
@@ -1,5 +1,8 @@
 package com.portal.centro.API.generic.crud;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+
 import java.util.List;
 
 public abstract class GenericService<T, ID> {
@@ -21,6 +24,14 @@ public abstract class GenericService<T, ID> {
 
     public List<T> getAll() {
         return genericRepository.findAll();
+    }
+
+    public List<T> page(PageRequest pageRequest) {
+        return genericRepository.findAll(pageRequest).getContent();
+    }
+
+    public List<T> search(Specification specification, PageRequest pageRequest) {
+        return genericRepository.findAll(specification, pageRequest).getContent();
     }
 
     public T findOneById(ID id) { return genericRepository.findById(id).orElseThrow(); }
