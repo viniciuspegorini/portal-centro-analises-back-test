977a3fb00e138b576c47fe11a71cf8f39c09ef17
cathula.oliveira <cathulapameladeoliveira@gmail.com>
2023-05-24 19:17:47 -0300

    carga incial de equipamentos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,17 +19,26 @@ INSERT INTO USERS (email, name, password, role) values ('fernanda@gmail.com', 'F
 INSERT INTO USERS (email, name, password, role) values ('cathula@gmail.com', 'Cathula', '$2a$10$ze2T2cQAxRjql2kXEwzSZux4UMTlt/4bP.Ma/oTdekyNOqpWAUB9C', 1);
 
 -- Carga inicial equipamentos
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('HPLC', 11.50, 21.00, 115.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('CG-EM', 15.00, 25.00, 150.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('TGA', 14.00, 22.00, 80.00);
-INSERT INTO equipment (name, value_sample_utfpr, value_sample_partner, value_sample_pf_pj) VALUES ('Calorímetro diferencial exploratório (DSC)', 30.00, 45.00, 110.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Microscópio Eletrônico de Varredura (MEV)', 17.00, 25.00, 80.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('DRX', 25.00, 45.00, 105.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrômetro de Infravermelho na região do próximo com Transformada de Fourier (FT-NIR)', 2.50, 5.00, 15.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrômetro de Infravermelho na região do Médio com Transformada de Fourier (FT-MIR)', 5.00, 15.00, 130.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrofotômetro de Absorção Molecular (UV-Vis)', 8.00, 15.00, 70.00);
-INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrômetro de Absorção Atômica', 18.00, 30.00, 80.00);
-INSERT INTO equipment (name, value_sample_utfpr, value_sample_partner, value_sample_pf_pj) VALUES ('A ELEMENTAR', 40.00, 60.00, 140.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Cromatógrafo líquido de alta eficiência - HPLC (modelo LC920, Varian)', 11.50, 21.00, 115.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Cromatógrafo a gás acoplado com espectrômetro de massa (CG-EM) (modelo
+431GC-210MS, Varian)', 15.00, 25.00, 150.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Analisador termogravimétrico - TGA-DTA-DSC (modelo SDT
+Q600, TA Instruments)', 14.00, 22.00, 80.00);
+INSERT INTO equipment (name, value_sample_utfpr, value_sample_partner, value_sample_pf_pj) VALUES ('Calorímetro diferencial exploratório - DSC (modelo Q20, TA
+instruments)', 30.00, 45.00, 110.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Microscópio Eletrônico de Varredura – MEV (modelo TM3000,
+Hitachi)', 17.00, 25.00, 80.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Difratômetro de Raios-X - DRX (modelo Miniflex 600, Rigaku)', 25.00, 45.00, 105.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrômetro de Infravermelho na região do próximo com
+Transformada de Fourier - FT-NIR (modelo MPA, BRUKER)', 2.50, 5.00, 15.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrômetro de Infravermelho na região do Médio com
+Transformada de Fourier - FT-MIR (modelo Frontier, Perkin Elmer)', 5.00, 15.00, 130.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrofotômetro de Absorção Molecular - UV-Vis (modelo
+LAMBDA 45, Perkin Elmer)', 8.00, 15.00, 70.00);
+INSERT INTO equipment (name, value_hour_utfpr, value_hour_partner, value_sample_pf_pj) VALUES ('Espectrômetro de Absorção Atômica - A ATÔMICA (modelo PinAAcle
+900T, Perkin Elmer)', 18.00, 30.00, 80.00);
+INSERT INTO equipment (name, value_sample_utfpr, value_sample_partner, value_sample_pf_pj) VALUES ('Analisador elementar de CHNS-O - A ELEMENTAR (modelo Euro EA,
+Euro Vector)', 40.00, 60.00, 140.00);
 
 INSERT INTO project (description, subject) VALUES ('PROJECT DESCRIPTION', 'PROJECT SUBJECT');
 
