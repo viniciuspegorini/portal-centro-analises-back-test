c898cdae949b1d2f3d087ef486ac68b1de2a0c66
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-06-05 20:01:23 -0300

    feat: audit

---
+++ src/main/java/com/portal/centro/API/repository/AuditRepository.java
@@ -0,0 +1,14 @@
+package com.portal.centro.API.repository;
+
+import com.portal.centro.API.generic.crud.GenericRepository;
+import com.portal.centro.API.model.Audit;
+import com.portal.centro.API.model.User;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AuditRepository extends GenericRepository<Audit, Long> {
+
+    List<Audit> findAllByUpdatedBy(User user);
+
+}
--- src/main/java/com/portal/centro/API/service/SolicitationService.java
+++ src/main/java/com/portal/centro/API/service/SolicitationService.java
@@ -3,19 +3,42 @@ package com.portal.centro.API.service;
 import com.portal.centro.API.enums.SolicitationStatus;
 import com.portal.centro.API.generic.crud.GenericRepository;
 import com.portal.centro.API.generic.crud.GenericService;
+import com.portal.centro.API.model.Audit;
 import com.portal.centro.API.model.Solicitation;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SolicitationService extends GenericService<Solicitation, Long> {
 
-    public SolicitationService(GenericRepository<Solicitation, Long> genericRepository) {
+    private final AuditService auditService;
+
+    public SolicitationService(GenericRepository<Solicitation, Long> genericRepository, AuditService auditService) {
         super(genericRepository);
+        this.auditService = auditService;
+    }
+
+    @Override
+    public Solicitation save(Solicitation requestBody) throws Exception {
+
+        Solicitation output = super.save(requestBody);
+        Audit audit = new Audit();
+        audit.setNewStatus(requestBody.getStatus());
+        audit.setSolicitation(output);
+
+        auditService.saveAudit(audit);
+
+        return output;
     }
 
     public Solicitation updateStatus(Long id, SolicitationStatus status) throws Exception {
         Solicitation solicitation = this.findOneById(id);
         solicitation.setStatus(status);
+
+        Audit audit = new Audit();
+        audit.setNewStatus(status);
+        audit.setSolicitation(solicitation);
+
+        auditService.saveAudit(audit);
 
         return super.save(solicitation);
     }
