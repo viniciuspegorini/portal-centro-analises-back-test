efeacf41f33eeeac48a5afd3b18603dd6685bdbd
marco-rozo <marcorozo99@gmail.com>
2023-06-13 19:09:27 -0300

    fix: add id in userDTO

--- src/main/java/com/portal/centro/API/dto/UserLoginDTO.java
+++ src/main/java/com/portal/centro/API/dto/UserLoginDTO.java
@@ -10,11 +10,13 @@ import lombok.Setter;
 public class UserLoginDTO {
 
     public UserLoginDTO(User user) {
+        this.id = user.getId();
         this.displayName = user.getName();
         this.email = user.getEmail();
         this.role = user.getRole();
     }
 
+    private Long id;
     private String displayName;
     private String email;
     private Type role;
