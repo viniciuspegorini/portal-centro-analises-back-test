b2e4396471c2e9792e7059a66ee961b87e90fc4a
Guilherme Minozzi <minozzi60@gmail.com>
2023-06-20 19:46:54 -0300

    fix(changePasswordDto): add notBlank on fields

--- src/main/java/com/portal/centro/API/dto/ChangePasswordDTO.java
+++ src/main/java/com/portal/centro/API/dto/ChangePasswordDTO.java
@@ -2,6 +2,8 @@ package com.portal.centro.API.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -9,8 +11,11 @@ import javax.validation.constraints.NotNull;
 public class ChangePasswordDTO {
 
   @NotNull
+  @NotBlank
   private String oldPassword;
+
   @NotNull
+  @NotBlank
   private String newPassword;
 
 }
