7d0c51853ad2d082a736ca9668124a7c300490b2
Guilherme Minozzi <minozzi60@gmail.com>
2023-06-20 20:15:16 -0300

    fix(changePasswordDto): add notEmpty and Size on password fields

--- src/main/java/com/portal/centro/API/dto/ChangePasswordDTO.java
+++ src/main/java/com/portal/centro/API/dto/ChangePasswordDTO.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -12,10 +14,14 @@ public class ChangePasswordDTO {
 
   @NotNull
   @NotBlank
+  @NotEmpty
+  @Size(min = 6, max = 254, message = "Old Password must be between 6 and 254 characters long")
   private String oldPassword;
 
   @NotNull
   @NotBlank
+  @NotEmpty
+  @Size(min = 6, max = 254, message = "New Password must be between 6 and 254 characters long")
   private String newPassword;
 
 }
